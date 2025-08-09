608f32150f01d3ca9c297075aa02841d4afb38fd
Nehmiya <nehmiyatech@gmail.com>
2025-08-09 19:04:23 +0300

    Banner

--- netflix-clone/src/Components/Banner/Banner.jsx
+++ netflix-clone/src/Components/Banner/Banner.jsx
@@ -1,12 +1,48 @@
-import React from 'react'
-import './Banner.css'
+import {React,useState,useEffect} from "react";
+import "./Banner.css";
+import axios from "../../Utils/axios"
+import requests from "../../Utils/Requests";
+import { hexToRgb } from "@mui/material/styles";
 
 function Banner() {
+    const [movie,setMovie] = useState({})
+    useEffect(()=>{
+       ( async()=>{
+            try {
+                const request = await axios.get(requests.fetchNetflixOriginals)
+                // console.log(request)
+                setMovie(request.data.results[
+                    Math.floor(Math.random()*request.data.results.length)
+                ])
+            } catch (error) {
+                console.error("Err: ",error)
+            }
+        })()
+    },[])
+
+
   return (
-    <div>
-      
+    <div
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url('https://image.tmdb.org/t/p/original${movie?.backdrop_path}')`,
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div className="banner-contents">
+        <h1 className="banner-title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <p className="banner-desc">{movie?.overview}</p>
+        <div className="banner-btns">
+          <button className="banner-btn play">Play</button>
+          <button className="banner-btn">My List</button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Banner
+export default Banner;
