bb556ca5caa702ac4f704936ea75674cd4ee645b
Nehmiya <nehmiyatech@gmail.com>
2025-08-09 19:23:22 +0300

    Banner

--- netflix-clone/src/Components/Banner/Banner.jsx
+++ netflix-clone/src/Components/Banner/Banner.jsx
@@ -1,36 +1,42 @@
-import {React,useState,useEffect} from "react";
+import { React, useState, useEffect } from "react";
 import "./Banner.css";
-import axios from "../../Utils/axios"
+import axios from "../../Utils/axios";
 import requests from "../../Utils/Requests";
-import { hexToRgb } from "@mui/material/styles";
 
 function Banner() {
-    const [movie,setMovie] = useState({})
-    useEffect(()=>{
-       ( async()=>{
-            try {
-                const request = await axios.get(requests.fetchNetflixOriginals)
-                // console.log(request)
-                setMovie(request.data.results[
-                    Math.floor(Math.random()*request.data.results.length)
-                ])
-            } catch (error) {
-                console.error("Err: ",error)
-            }
-        })()
-    },[])
+  const [movies, setMovies] = useState([]);
+  const [movieIndex, setMovieIndex] = useState(0);
 
-    const truncate = (str,n) => str?.length > n ? str.substr(0,n-1) + '...' : str 
+  useEffect(() => {
+    (async () => {
+      try {
+        const request = await axios.get(requests.fetchNetflixOriginals);
+        setMovies(request.data.results);
+        setMovieIndex(0);
+      } catch (error) {
+        console.error("Error fetching banner data:", error);
+      }
+    })();
+  }, []);
 
+  const truncate = (str, n) =>
+    str?.length > n ? str.substr(0, n - 1) + "..." : str;
+
+  const handleNext = () => {
+    setMovieIndex((prevIndex) =>
+      prevIndex + 1 < movies.length ? prevIndex + 1 : 0
+    );
+  };
+
+  const movie = movies[movieIndex];
 
   return (
     <div
       className="banner"
       style={{
         backgroundSize: "cover",
-        backgroundImage: `url('https://image.tmdb.org/t/p/original${movie?.backdrop_path}')`,
+        backgroundImage: `url("https://image.tmdb.org/t/p/original${movie?.backdrop_path}")`,
         backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
       }}
     >
       <div className="banner-contents">
@@ -42,8 +48,12 @@ function Banner() {
         <div className="banner-btns">
           <button className="banner-btn play">Play</button>
           <button className="banner-btn">My List</button>
+          <button className="banner-btn" onClick={handleNext}>
+            Next
+          </button>
         </div>
       </div>
+      <div className="banner-fadeBottom" />
     </div>
   );
 }
