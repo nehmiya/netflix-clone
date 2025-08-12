308089edf08cef8f75535efed8f9ad4008ece9dc
Nehmiya <nehmiyatech@gmail.com>
2025-08-12 22:14:06 +0300

    Finished the project

--- netflix-clone/src/Components/Rows/Row/Row.jsx
+++ netflix-clone/src/Components/Rows/Row/Row.jsx
@@ -1,33 +1,79 @@
-import React ,{useState,useEffect}from 'react'
-import axios from '../../../Utils/axios'
-import './Row.css'
+import React, { useState, useEffect } from "react";
+import axios from "../../../Utils/axios";
+import "./Row.css";
+import movieTrailer from "movie-trailer";
+import YouTube from "react-youtube";
 
+function Row({ title, fetchURL, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+  const [showTrailer, setShowTrailer] = useState(false);
 
+  const base_url = "https://image.tmdb.org/t/p/original";
 
-function Row({title,fetchURL,isLargeRow}) {
+  useEffect(() => {
+    (async () => {
+      try {
+        const request = await axios.get(fetchURL);
+        setMovies(request.data.results);
+      } catch (error) {
+        console.error(`Err: ${error}`);
+      }
+    })();
+  }, [fetchURL]);
 
-    const [movies,setMovies] = useState([])
+  const handleClick = (movie) => {
+    if (showTrailer) {
+      setShowTrailer(false);
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.title || movie?.name || movie?.original_name)
+        .then((url) => {
+          const urlparams = new URLSearchParams(new URL(url).search);
+          setTrailerUrl(urlparams.get("v"));
+          setShowTrailer(true);
+        })
+        .catch(() => {
+          console.error("No trailer found for", movie?.title);
+        });
+    }
+  };
 
-    // const base_url = "https://image.tmdb.org/t/p/original"
-
-    useEffect(()=>{
-        (async()=>{
-            try {
-                console.log(fetchURL)
-                const request = await axios.get(fetchURL);
-                console.log(request)
-                setMovies(request.data.results)
-            } catch (error) {
-                console.error(`Err: ${error}`)
-            }
-        })()
-    },[])
+  const opts = {
+    height: "480",
+    width: "100%",
+    playerVars: { autoplay: 1 },
+  };
 
   return (
-    <div>
-        {console.log(movies)}
+    <div className="row">
+      <h1>{title}</h1>
+      <div className="row-posters">
+        {movies?.map((movie, index) => (
+          <img
+            onClick={() => handleClick(movie)}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+            key={index}
+            className={`row-poster ${isLargeRow && "row-poster-large"}`}
+          />
+        ))}
+      </div>
+
+      {showTrailer && (
+        <div className="trailer-overlay">
+          <div className="trailer-container">
+            <button className="close-btn" onClick={() => setShowTrailer(false)}>
+              ✖
+            </button>
+            <YouTube videoId={trailerUrl} opts={opts} />
+          </div>
+        </div>
+      )}
     </div>
-  )
+  );
 }
 
-export default Row
+export default Row;
--- netflix-clone/src/Components/Rows/RowList/RowList.jsx
+++ netflix-clone/src/Components/Rows/RowList/RowList.jsx
@@ -1,14 +1,39 @@
-import React from 'react'
-import Row from '../Row/Row'
-import requests from '../../../Utils/Requests';
+import React, { useState, useEffect } from "react";
+import Row from "../Row/Row";
+import requests from "../../../Utils/Requests";
 
 function RowList() {
+  const [isLarge, setIsLarge] = useState(window.innerWidth >= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLarge(window.innerWidth >= 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <div>
-      <Row title="Netflix Originals" fetchURL={requests.fetchNetflixOriginals}/>
-      <Row title="Trending" fetchURL={requests.fetchTrending}/>
+      <Row
+        title="Netflix Originals"
+        fetchURL={requests.fetchNetflixOriginals}
+        isLargeRow={isLarge}
+      />
+      <Row
+        title="Trending Now"
+        fetchURL={requests.fetchTrending}
+        isLargeRow={isLarge}
+      />
+     <Row title={'TV Shows'} fetchURL={requests.fetchTvShow} isLargeRow={isLarge}/>
+      <Row title={'Action Movies'} fetchURL={requests.fetchActionMovies} isLargeRow={isLarge}/>
+      <Row title={'Comedy Movies'} fetchURL={requests.fetchComedyMovies} isLargeRow={isLarge}/>
+      <Row title={'Horror Movies'} fetchURL={requests.fetchHorrorMovies} isLargeRow={isLarge}/>
+      <Row title={'Documentaries'} fetchURL={requests.fetchDocumentaries} isLargeRow={isLarge}/>
+      
     </div>
   );
 }
 
-export default RowList
+export default RowList;
